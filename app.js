11b73393216a1f508383c5d557a314a1d97ea689
msshamsa <maryam.s.shamsa@gmail.com>
2020-07-24 23:25:08 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req, res){
+res.sendFile(__dirname + "/signup.html");
+});
+app.post("/",function(req,res){
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    console.log(firstName,lastName,email);
+    const data = {
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url="https://us17.api.mailchimp.com/3.0/lists/2048caefff";
+    const options={
+        method:"POST",
+        auth:"mary1:e1064bc41cd5f965948ce624dcfff8f3-us17"
+
+    }
+  const request=  https.request(url, options, function(response){
+      response.on("data",function(data){
+          console.log(JSON.parse(data));
+      })
+
+
+    })
+request.write(jsonData);
+request.end();
+});
+
+
+app.listen(3000,function(){
+    console.log("this app is running on port 3000");
+});
+//API key
+//e1064bc41cd5f965948ce624dcfff8f3-us17
+//list id
+//2048caefff
